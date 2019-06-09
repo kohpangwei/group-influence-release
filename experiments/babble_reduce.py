0ce11962cb0ead549baab66057261ad187c6c8b1
Kai-Siang Ang <kaiang@stanford.edu>
2019-06-09 14:46:34 -0700

    Paring down babble

--- experiments/babble_reduce.py
+++ experiments/babble_reduce.py
@@ -34,18 +34,17 @@ class BabbleReduce(Experiment):
         self.test = self.datasets.test
         self.validation = self.datasets.validation
         self.num_train = self.datasets.train.num_examples
-        self.target_num_features = self.config['target_num_features'] # not actually used
         self.fit_intercept = self.config['fit_intercept']
         self.regularization_type = self.config['regularization_type']
+        self.save_reduced_dir = self.config['save_reduced_dir']
         assert self.regularization_type in ['l1','l2']
 
-    experiment_id = "babble_reduce_reruns" #TODO
+    experiment_id = "babble_reduce"
 
     @property
     def run_id(self):
-        return "{}_target_num_features-{}_regularization_type-{}_sample_weights-{}".format(
+        return "{}_regularization_type-{}_sample_weights-{}".format(
             self.config['dataset_config']['dataset_id'],
-            self.config['target_num_features'],
             self.config['regularization_type'],
             self.config['sample_weights'])
 
@@ -139,102 +138,8 @@ class BabbleReduce(Experiment):
             name = 'reduced_{}_nonfires_x'.format(self.config['dataset_config']['dataset_id'])
             if self.config['sample_weights']: name += '_via_sample_weights'
             res['reduced_features_nonfires'] = reduced_nonfires
-            np.savez(os.path.join(self.out_dir, name),
+            np.savez(os.path.join(self.save_reduced_dir, name),
                     reduced_x=[reduced_nonfires])
-            print('Saved reduced nonfires to {}'.format(os.path.join(self.out_dir, name)))
+            print('Saved reduced nonfires to {}'.format(os.path.join(self.save_reduced_dir, name)))
         return res
 
-    @phase(2)
-    def compare_to_l2(self):
-        res = dict()
-        if (self.regularization_type != 'l2'):
-            config_copy = copy(self.config['dataset_config'])
-            config_copy['dataset_id'] = 'reduced_' + config_copy['dataset_id']
-            reduced_ds = ds.loader.load_dataset(**config_copy)
-            self.train = reduced_ds.train
-            self.validation = reduced_ds.validation
-            self.test = reduced_ds.test
-
-            reg_min, reg_max, reg_samples = self.config['normalized_cross_validation_range']
-            reg_min *= self.num_train
-            reg_max *= self.num_train
-            regs = np.logspace(np.log10(reg_min), np.log10(reg_max), reg_samples)
-
-            # We don't do true cross validation; just use validation set.
-            cv_accs = [np.zeros_like(regs) for i in range(3)]
-            cv_coefs = [None for i in range(len(regs))]
-            cv_intercepts = np.zeros_like(regs)
-            cv_f1s = [np.zeros_like(regs) for i in range(3)]
-
-            for i, reg in enumerate(regs):
-                with benchmark("Evaluating sklearn l2 model for reg={}".format(reg)):
-                    np.random.seed(0)
-                    l2_model = sklearn.linear_model.LogisticRegression(penalty='l2', fit_intercept=self.fit_intercept, random_state=np.random.RandomState(2)) #TODO
-                    l2_model.set_params(C=1.0/reg)
-
-                    l2_model.fit(self.train.x, self.train.labels,
-                            sample_weight=self.sample_weights[0])
-
-                    cv_coefs[i] = l2_model.coef_[0]
-                    #print('Coefs: {}'.format(cv_coefs[i]))
-                    cv_intercepts[i] = l2_model.intercept_ if self.fit_intercept else 0
-                    print('Intercept: {}'.format(cv_intercepts[i]))
-
-                    cv_accs[0][i] = l2_model.score(self.train.x, self.train.labels,
-                            sample_weight=self.sample_weights[0])
-                    cv_accs[1][i] = l2_model.score(self.validation.x, self.validation.labels,
-                            sample_weight=self.sample_weights[1])
-                    cv_accs[2][i] = l2_model.score(self.test.x, self.test.labels,
-                            sample_weight=self.sample_weights[2])
-                    print('Train acc: {}'.format(cv_accs[0][i]))
-                    print('Validation acc: {}'.format(cv_accs[1][i]))
-                    print('Test acc: {}'.format(cv_accs[2][i]))
-
-                    y_preds = [None for j in range(3)]
-                    y_preds[0] = l2_model.predict(self.train.x)
-                    y_preds[1] = l2_model.predict(self.validation.x)
-                    y_preds[2] = l2_model.predict(self.test.x)
-
-                    cv_f1s[0][i] = f1_score(self.train.labels, y_preds[0],
-                            sample_weight=self.sample_weights[0])
-                    cv_f1s[1][i] = f1_score(self.validation.labels, y_preds[1],
-                            sample_weight=self.sample_weights[1])
-                    cv_f1s[2][i] = f1_score(self.test.labels, y_preds[2],
-                            sample_weight=self.sample_weights[2])
-                    print('Train f1: {}'.format(cv_f1s[0][i]))
-                    print('Validation f1: {}'.format(cv_f1s[1][i]))
-                    print('Test f1: {}'.format(cv_f1s[2][i]))
-                    print(f1_score(self.test.labels, np.array(l2_model.predict(self.test.x)>0.5).astype(np.int), sample_weight=self.sample_weights[2]))
-
-                    if reg == 2.4177:
-                        print(self.train.x.shape, reg, self.sample_weights[0])
-                        print(l2_model.get_params()) #TODO
-                        print(l2_model.coef_, l2_model.intercept_)
-                        return
-
-
-
-            best_CV_i = np.argmax(cv_f1s[1])
-            best_l2_reg = regs[best_CV_i]
-            cv_accs = np.array(cv_accs)
-            cv_f1s = np.array(cv_f1s)
-            print('Selecting l2 regularization {}, with accs {} and f1s {}.'.format(best_l2_reg, cv_accs[:, best_CV_i], cv_f1s[:,best_CV_i]))
-            #print('L2 coefs {}'. format(cv_coefs[best_CV_i]))
-
-            best_i = self.R['best_i']
-            feature_indices = self.R['feature_inds']
-            l1_coefs = self.R['coefs'][best_i][feature_indices]
-            l1_accs = self.R['accs'][:,best_i]
-            l1_f1s = self.R['f1s'][:,best_i]
-            #print('Nonzero coeffs under l1 were {}.'.format(l1_coefs))
-            print('L1 model had accs {} and f1s {}.'.format(l1_accs, l1_f1s))
-
-            res['cv_best_i'] = best_CV_i
-            res['cv_regs'] = regs
-            res['cv_accs'] = np.array(cv_accs)
-            res['cv_f1s'] = np.array(cv_f1s)
-            res['cv_l2_reg'] = best_l2_reg
-            res['cv_coefs'] = np.array(cv_coefs)
-            res['cv_intercepts'] = cv_intercepts
-    
-        return res
--- scripts/babble_reduce.py
+++ scripts/babble_reduce.py
@@ -16,6 +16,8 @@ if __name__ == "__main__":
                         help="The experiment output directory")
     parser.add_argument('--max-memory', default=1e9, type=int,
                         help="A rule-of-thumb estimate of the GPU memory capacity")
+    parser.add_argument('--save-reduced-dir', default=None, type=str,
+                        help="The directory to save the reduced dataset")
 
     # Execution args
     parser.add_argument('--force-refresh', dest='force_refresh', action='store_true',
@@ -25,14 +27,10 @@ if __name__ == "__main__":
     parser.set_defaults(force_refresh=False)
 
     # Experiment args
-    parser.add_argument('--dataset-id', default="spouse", type=str,
+    parser.add_argument('--dataset-id', default="cdr", type=str,
                         help="The dataset to use")
-    parser.add_argument('--target-num-features', default=4000, type=int,
-                        help="Target number of features to reduce to")
     parser.add_argument('--fit-intercept', dest='fit_intercept', action='store_true',
                         help="Whether the logistic regression should fit intercept")
-    parser.add_argument('--regularization-type', default="l1", type=str,
-                        help="Using l1 or l2 regularization to sparsify.")
     parser.add_argument('--sample-weights', dest='sample_weights', action='store_true',
                         help="Whether to weight examples according to # of LF contributions")
     parser.set_defaults(fit_intercept=True, sample_weights=True)
@@ -47,18 +45,16 @@ if __name__ == "__main__":
     config = {
         'dataset_config': dataset_config,
         'normalized_cross_validation_range': {
-            'spouse': (1e-7, 1, 10),
             'cdr': (1e-4, 1e-1, 10),
         }[args.dataset_id],
         'initial_reg_range': {
-            'spouse': (1e-13, 1e3, 10),
-            'cdr': (1e-13,1e3,17),
+            'cdr': (1e-13, 1e3, 17),
         }[args.dataset_id],
         'max_memory': int(args.max_memory),
-        'target_num_features': args.target_num_features,
         'fit_intercept': args.fit_intercept,
-        'regularization_type': args.regularization_type,
+        'regularization_type': 'l1',
         'sample_weights': args.sample_weights,
+        'save_reduced_dir': args.save_reduced_dir,
     }
 
     exp = BabbleReduce(config, out_dir=args.out_dir)
