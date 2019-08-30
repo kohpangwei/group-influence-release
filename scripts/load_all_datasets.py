f0ac6d2775e2b19b9e68b6b068d3e45be5ba9ce1
Hubert Teo <hubert.teo.hk@gmail.com>
2019-08-29 17:18:22 -0700

    Allow individual dataset loading

--- scripts/load_all_datasets.py
+++ scripts/load_all_datasets.py
@@ -1,7 +1,7 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import unicode_literals  
+from __future__ import unicode_literals
 
 import datasets.mnist
 import datasets.spam
@@ -11,10 +11,8 @@ import datasets.loader
 
 import argparse
 
-# NOTE: YOU MUST RUN scripts/babble_reduce.py FIRST
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Compute subset self and test influences')
+    parser = argparse.ArgumentParser(description='Load all datasets to ensure they work.')
 
     # Dataset args
     parser.add_argument('--data-dir', default=None, type=str,
@@ -23,12 +21,22 @@ if __name__ == "__main__":
                         help="Center the dataset")
     parser.add_argument('--append-bias', dest='append_bias', action='store_true',
                         help="Append a bias component to the dataset")
+    parser.add_argument('--dataset-ids', nargs='+', type=str, default=None,
+                        help="The IDs of the datasets to load. Defaults to all datasets.")
+    parser.add_argument('--supplement-ids', nargs='+', type=str, default=None,
+                        help="The IDs of the supplements to load. Defaults to all supplements.")
     parser.set_defaults(center_data=False, append_bias=False)
 
     args = parser.parse_args()
 
-    dataset_ids = datasets.loader.DATASETS.keys()
+    dataset_ids = args.dataset_ids
+    if len(dataset_ids) == 0:
+        dataset_ids = datasets.loader.DATASETS.keys()
+
     for dataset_id in dataset_ids:
+        if dataset_id not in datasets.loader.DATASETS:
+            print("Unknown dataset ID: {}".format(dataset_id))
+            continue
         print("Loading {}".format(dataset_id))
         dataset = datasets.loader.load_dataset(dataset_id=dataset_id,
                                                center_data=args.center_data,
@@ -41,9 +49,14 @@ if __name__ == "__main__":
             None if dataset.validation is None else "{}".format(dataset.validation.x.shape),
             None if dataset.test is None else "{}".format(dataset.test.x.shape)))
 
-    supplement_ids = datasets.loader.SUPPLEMENTS.keys()
+    supplement_ids = args.supplement_ids
+    if len(supplement_ids) == 0:
+        supplement_ids = datasets.loader.SUPPLEMENTS.keys()
+
     for supplement_id in supplement_ids:
+        if supplement_id not in datasets.loader.SUPPLEMENTS:
+            print("Unknown supplement ID: {}".format(supplement_id))
+            continue
         print("Loading {}".format(supplement_id))
-        
         supplement = datasets.loader.load_supplemental_info(supplement_id, data_dir=args.data_dir)
         print("Supplement contains {}".format(supplement))
