ee74d6e6ab85f1f1d906532b466e07529f6b48fe
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 10:01:20 +0000

    Adding tarfile member sanitization to extractall()

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -79,7 +79,26 @@ def load_cifar10(validation_size=1000, data_dir=None):
 
         print('Extracting {}.'.format(raw_cifar10_path))
         with tarfile.open(raw_cifar10_path, 'r:gz') as tarf:
-            tarf.extractall(path=dataset_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tarf, path=dataset_dir)
 
         # no one-hot encoding of labels
         train_images, train_labels, _ = _load_training_data()
--- datasets/spam.py
+++ datasets/spam.py
@@ -68,7 +68,26 @@ def load_spam(truncate=None, data_dir=None):
 
         print("Extracting {}".format(raw_spam_path))
         with tarfile.open(raw_spam_path, 'r:gz') as tarf:
-            tarf.extractall(path=dataset_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tarf, path=dataset_dir)
 
         print("Processing spam")
         X_train, Y_train, X_valid, Y_valid, X_test, Y_test = process_spam(dataset_dir, truncate)
